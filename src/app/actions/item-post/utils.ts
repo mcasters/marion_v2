abc5c2402955fff3a931fe543015aea66665dbd4
Marion <contact@marioncasters.fr>
2026-03-04 10:58:30 +0100

    End actions modifications

--- src/app/actions/item-post/utils.ts
+++ src/app/actions/item-post/utils.ts
@@ -5,6 +5,7 @@ import {
   getEmptyPost,
   getEmptyWork,
   getNoCategory,
+  transformValueToKey,
 } from "@/lib/utils/commonUtils.ts";
 
 export const createPaintingData = async (
@@ -112,6 +113,33 @@ type FileInfo = {
   width: number;
   height: number;
   isMain: boolean;
+};
+
+export const createCategoryData = (formData: FormData) => {
+  const rawFormData = Object.fromEntries(formData);
+  const value = rawFormData.value as string;
+  const isUpdate = rawFormData.id !== "0";
+  const contentData = {
+    title: rawFormData.title as string,
+    text: rawFormData.text as string,
+    imageFilename: rawFormData.filename as string,
+    imageWidth: Number(rawFormData.width),
+    imageHeight: Number(rawFormData.height),
+  };
+
+  const content = isUpdate
+    ? {
+        update: contentData,
+      }
+    : {
+        create: contentData,
+      };
+
+  return {
+    key: transformValueToKey(value),
+    value,
+    content,
+  };
 };
 
 export const saveFiles = async (
