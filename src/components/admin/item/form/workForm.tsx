5f8c4e0ebf5a8201c372db10fb9bd6e82f841077
Marion <contact@marioncasters.fr>
2026-03-06 12:24:39 +0100

    sold/isToSell work prop managing

--- src/components/admin/item/form/workForm.tsx
+++ src/components/admin/item/form/workForm.tsx
@@ -63,158 +63,201 @@ export default function WorkForm({ work, onClose, categories }: Props) {
           />
         </>
       )}
-      <input name="isToSell" type="hidden" value={String(work.isToSell)} />
       <input
         name="oldCategoryId"
         type="hidden"
         value={String(work.categoryId)}
       />
-      <div className={s.column}>
-        <input
-          name="title"
-          type="text"
-          placeholder="Titre"
-          value={workItem.title}
-          onChange={(e) => setWorkItem({ ...workItem, title: e.target.value })}
-          autoFocus
-          required
-        />
-        <select
-          name="categoryId"
-          value={workItem.categoryId?.toString()}
-          onChange={(e) => {
-            setWorkItem(
-              Object.assign({}, workItem, {
-                categoryId: e.target.value,
-              }),
-            );
-          }}
-        >
-          <option value={0}>-- Aucune catégorie --</option>
-          {categories &&
-            categories.map((cat) => {
-              if (cat.value !== "Sans catégorie")
-                return (
-                  <option key={cat.id} value={cat.id}>
-                    {cat.value}
-                  </option>
-                );
-            })}
-        </select>
-        <input
-          name="date"
-          type="date"
-          placeholder="Date"
-          value={date}
-          onChange={(e) => {
-            setDate(e.target.value);
-          }}
-          required
-        />
-        <input
-          name="technique"
-          type="text"
-          placeholder="Technique"
-          value={workItem.technique}
-          onChange={(e) =>
-            setWorkItem({ ...workItem, technique: e.target.value })
-          }
-          required
-        />
-        <div className={s.dimensions}>
+      <div className={s.columnWrapper}>
+        <div>
           <input
-            name="height"
-            type="number"
-            placeholder="Hauteur (cm)"
-            value={workItem.height === 0 ? "" : workItem.height.toString()}
+            name="title"
+            type="text"
+            placeholder="Titre"
+            value={workItem.title}
             onChange={(e) =>
-              setWorkItem({ ...workItem, height: Number(e.target.value) })
+              setWorkItem({ ...workItem, title: e.target.value })
             }
+            autoFocus
             required
           />
           <input
-            name="width"
-            type="number"
-            placeholder="Largeur (cm)"
-            value={workItem.width === 0 ? "" : workItem.width.toString()}
-            onChange={(e) =>
-              setWorkItem({ ...workItem, width: Number(e.target.value) })
-            }
+            name="date"
+            type="date"
+            placeholder="Date"
+            value={date}
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
             required
           />
-          {isSculpture && (
+          <div className={s.dimensions}>
             <input
-              name="length"
+              name="height"
               type="number"
-              placeholder="Profondeur (cm)"
-              value={workItem.length === 0 ? "" : workItem.length.toString()}
+              placeholder="Hauteur (cm)"
+              value={workItem.height === 0 ? "" : workItem.height.toString()}
               onChange={(e) =>
-                setWorkItem({ ...workItem, length: Number(e.target.value) })
+                setWorkItem({ ...workItem, height: Number(e.target.value) })
               }
               required
             />
-          )}
-        </div>
-        <div className={s.checkTextForm}>
-          <label className="checkboxLabel">
             <input
-              name="isToSell"
-              type="checkbox"
-              checked={workItem.isToSell}
-              onChange={(e) =>
-                setWorkItem({ ...workItem, isToSell: e.target.checked })
-              }
-            />
-            À vendre
-          </label>
-          {workItem.isToSell && (
-            <input
-              name="price"
+              name="width"
               type="number"
-              placeholder="Prix (facultatif)"
-              value={
-                !workItem.price || workItem.price === 0
-                  ? ""
-                  : workItem.price.toString()
-              }
+              placeholder="Largeur (cm)"
+              value={workItem.width === 0 ? "" : workItem.width.toString()}
               onChange={(e) =>
-                setWorkItem({ ...workItem, price: Number(e.target.value) })
+                setWorkItem({ ...workItem, width: Number(e.target.value) })
               }
+              required
             />
-          )}
+            {isSculpture && (
+              <input
+                name="length"
+                type="number"
+                placeholder="Profondeur (cm)"
+                value={workItem.length === 0 ? "" : workItem.length.toString()}
+                onChange={(e) =>
+                  setWorkItem({ ...workItem, length: Number(e.target.value) })
+                }
+                required
+              />
+            )}
+          </div>
+          <textarea
+            name="description"
+            placeholder="Description (facultative)"
+            value={workItem.description}
+            onChange={(e) =>
+              setWorkItem({ ...workItem, description: e.target.value })
+            }
+            rows={3}
+          />
         </div>
-        <textarea
-          name="description"
-          placeholder="Description (facultative)"
-          value={workItem.description}
-          onChange={(e) =>
-            setWorkItem({ ...workItem, description: e.target.value })
-          }
-          rows={3}
-        />
-        <div className={s.checkTextForm}>
-          <label className="checkboxLabel">
-            <input
-              name="isOut"
-              type="checkbox"
-              checked={workItem.isOut}
-              onChange={(e) =>
-                setWorkItem({ ...workItem, isOut: e.target.checked })
-              }
-            />
-            Sortie
-          </label>
-          {workItem.isOut && (
-            <textarea
-              name="outInformation"
-              placeholder="Information de sortie (facultative)"
-              value={workItem.outInformation}
-              onChange={(e) =>
-                setWorkItem({ ...workItem, outInformation: e.target.value })
-              }
-              rows={3}
-            />
-          )}
+        <div className={s.col2}>
+          <select
+            name="categoryId"
+            value={workItem.categoryId?.toString()}
+            onChange={(e) => {
+              setWorkItem(
+                Object.assign({}, workItem, {
+                  categoryId: e.target.value,
+                }),
+              );
+            }}
+          >
+            <option value={0}>-- Aucune catégorie --</option>
+            {categories &&
+              categories.map((cat) => {
+                if (cat.value !== "Sans catégorie")
+                  return (
+                    <option key={cat.id} value={cat.id}>
+                      {cat.value}
+                    </option>
+                  );
+              })}
+          </select>
+          <input
+            name="technique"
+            type="text"
+            placeholder="Technique"
+            value={workItem.technique}
+            onChange={(e) =>
+              setWorkItem({ ...workItem, technique: e.target.value })
+            }
+            required
+          />
+          <div className={s.checkTextForm}>
+            <label className="checkboxLabel">
+              <input
+                name="isToSell"
+                type="checkbox"
+                checked={workItem.isToSell}
+                onChange={(e) => {
+                  const isToSell = e.target.checked;
+                  setWorkItem({
+                    ...workItem,
+                    isToSell,
+                    sold: !isToSell,
+                  });
+                }}
+              />
+              À vendre
+            </label>
+            {workItem.isToSell && (
+              <input
+                name="price"
+                type="number"
+                placeholder="Prix (facultatif)"
+                value={
+                  !workItem.price || workItem.price === 0
+                    ? ""
+                    : workItem.price.toString()
+                }
+                onChange={(e) =>
+                  setWorkItem({ ...workItem, price: Number(e.target.value) })
+                }
+              />
+            )}
+          </div>
+          <div className={s.checkTextForm}>
+            <label className="checkboxLabel">
+              <input
+                name="sold"
+                type="checkbox"
+                checked={workItem.sold}
+                onChange={(e) => {
+                  const sold = e.target.checked;
+                  setWorkItem({
+                    ...workItem,
+                    sold,
+                    isToSell: !sold,
+                  });
+                }}
+              />
+              Vendu
+            </label>
+            {workItem.sold && (
+              <input
+                name="price"
+                type="number"
+                placeholder="Prix (facultatif)"
+                value={
+                  !workItem.price || workItem.price === 0
+                    ? ""
+                    : workItem.price.toString()
+                }
+                onChange={(e) =>
+                  setWorkItem({ ...workItem, price: Number(e.target.value) })
+                }
+              />
+            )}
+          </div>
+          <div className={s.checkTextForm}>
+            <label className="checkboxLabel">
+              <input
+                name="isOut"
+                type="checkbox"
+                checked={workItem.isOut}
+                onChange={(e) =>
+                  setWorkItem({ ...workItem, isOut: e.target.checked })
+                }
+              />
+              Sortie
+            </label>
+            {workItem.isOut && (
+              <textarea
+                name="outInformation"
+                placeholder="Information de sortie (facultative)"
+                value={workItem.outInformation}
+                onChange={(e) =>
+                  setWorkItem({ ...workItem, outInformation: e.target.value })
+                }
+                rows={3}
+              />
+            )}
+          </div>
         </div>
       </div>
       <Preview
