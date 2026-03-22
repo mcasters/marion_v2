9cd03ae4415a58f2690cee9fc0bc9d950587dd64
Marion <contact@marioncasters.fr>
2026-03-22 20:49:49 +0100

    Fix bug on suppress item

--- src/components/admin/common/selectableList/selectableListRow.tsx
+++ src/components/admin/common/selectableList/selectableListRow.tsx
@@ -47,7 +47,7 @@ export default function SelectableListRow({
         )}
       </span>
       <span className={s.itemIcon}>
-        <DeleteButton onDelete={deleteAction} disabled={!deleteAction} />
+        <DeleteButton deleteAction={deleteAction} disabled={!deleteAction} />
       </span>
     </>
   );
